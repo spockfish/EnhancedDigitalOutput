f64c9aaf3344fc49794ffd015f1667a0e2e0e39c
Harry ten Berge <h.tenberge@xablu.com>
2016-11-19 20:38:23 +0100

    change buffer size

--- edo/settings_EnhancedDigitalOutput.lua
+++ edo/settings_EnhancedDigitalOutput.lua
@@ -1,1 +1,1 @@
-settings = {autoKernelUpdate=false,firstUse=false,periodCount=4,embeddedTTHack=false,cpuIdleFullspeed=false,bufferTime=999999999,playbackDevice="TXRX",}
+settings = {autoKernelUpdate=false,firstUse=false,periodCount=4,embeddedTTHack=false,cpuIdleFullspeed=false,bufferTime=100000,playbackDevice="TXRX",}
